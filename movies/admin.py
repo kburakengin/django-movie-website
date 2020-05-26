a91cdfba554dcd8314776ac95ff0247e958d8bbb
Kadir Burak Engin <39514824+Kburak@users.noreply.github.com>
2020-05-26 22:18:23 +0300

    deleted comments

--- movies/admin.py
+++ movies/admin.py
@@ -5,13 +5,11 @@ from .models import Movies
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('id','name','created_date','is_published')
-    list_display_links = ('id','name')  # link olusturma
-    list_filter = ('created_date',) # filtreleme islemi
-    list_editable = ('is_published',)  # editable alan , tick ekle kaldir
+    list_display_links = ('id','name')
+    list_filter = ('created_date',)
+    list_editable = ('is_published',)
     search_fields = ('name', 'description')
     list_per_page = 20
-
-
 
 
 admin.site.register(Movies, MovieAdmin)
